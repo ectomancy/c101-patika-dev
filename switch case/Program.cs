344d4228483c6386802678889e2048c046ca904f
ectomancy <98704895+ectomancy@users.noreply.github.com>
2022-04-01 23:38:59 +0300

    switch case

---
+++ switch case/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _07_switchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ay = DateTime.Now.Month;
+            switch (ay)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındasınız");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayındasınız");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayıs ayındasınız");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış verindasınız");
+                    break;
+            }
+
+            switch (ay)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    Console.WriteLine("Kış Mevsimi");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar Mevsimi");
+                    break;
+                default:
+                    Console.WriteLine("Mevsim Belirsiz");
+                    break;
+
+            }
+        }
+    }
+}
