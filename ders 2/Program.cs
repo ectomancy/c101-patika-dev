19a12b6264318b35cb1e4b2ed6173b2773f832ad
ectomancy <98704895+ectomancy@users.noreply.github.com>
2022-04-01 20:12:02 +0300

    operatörler

---
+++ ders 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ders_2
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            int x = 3;
+            int y = 3;
+            y = y+2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 2;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            bool isSuccess = true;
+            bool isCompleted =false;
+             
+             if(isSuccess && isCompleted)
+             Console.WriteLine("perfect");
+             if(isSuccess || isCompleted)
+             Console.WriteLine("Great");
+
+             int a = 3;
+             int b= 4;
+
+             bool sonuc = a<b;
+             Console.WriteLine(sonuc);
+             sonuc = a>b;
+             Console.WriteLine(sonuc);
+             sonuc = a>=b;
+             Console.WriteLine(sonuc);
+             sonuc = a<=b;
+             Console.WriteLine(sonuc);
+             sonuc = a!=b;
+             Console.WriteLine(sonuc);
+             sonuc = a==b;
+             Console.WriteLine(sonuc);
+             
+
+        }
+    }
+}
