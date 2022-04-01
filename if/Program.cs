42649de706a4ed112e7f743bfc7d163f8f771593
ectomancy <98704895+ectomancy@users.noreply.github.com>
2022-04-01 23:14:25 +0300

    if

---
+++ if/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int Time = DateTime.Now.Hour;
+            
+            if(Time>=6 && Time<11)
+            Console.WriteLine("günaydın!");
+
+            else if(Time>=10 && Time<=18)
+            Console.WriteLine("iyi akşamlar!");
+             
+             else if(Time>=20)
+             Console.WriteLine("iyi geceler!");
+
+             string sonuc = Time<=18 ? "iyi günler!" : "iyi geceler";
+             
+          
+
+
+
+        }
+    }
+}
