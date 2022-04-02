4935b0f3de403f33780b429537696b10c81babaf
ectomancy <98704895+ectomancy@users.noreply.github.com>
2022-04-02 23:20:32 +0300

    while  Foreach

---
+++ while foreach/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac<= sayi)
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.WriteLine(character);
+                character ++;
+            }
+
+            string[] arabalar = {"BMW","ford","toyota","Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
