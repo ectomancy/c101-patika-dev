b3024ad11bed801927260d6b218b78605be0a20a
ectomancy <98704895+ectomancy@users.noreply.github.com>
2022-04-04 00:04:37 +0300

    c#101 1. soru d

--- c# 101 ödev 2/1. soru/Program.cs
+++ c# 101 ödev 2/1. soru/Program.cs
@@ -1,2 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar_Soru_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList asalListe = new ArrayList();
+            ArrayList antiasalListe = new ArrayList();
+            
+            Console.WriteLine("20 Tane pozitif sayı giriniz.");
+            
+            for (int i = 1; i < 21; i++)
+            {
+                Console.Write("{0}. sayıyı giriniz: ", i);
+                int sayi = int.Parse(Console.ReadLine());
+                if(sayi < 0)
+                  Console.WriteLine("Pozitif sayı giriniz");
+
+                else if(sayi == 0)
+                  Console.WriteLine("Pozitif sayı giriniz");
+                
+                else if(sayi % 2 !=0 && sayi % 3 !=0 && sayi % 5 !=0 && sayi % 7 !=0)
+                asalListe.Add(sayi);
+                
+                else if(sayi == 2)
+                asalListe.Add(2);
+                
+                else if(sayi == 3)
+                asalListe.Add(3);
+
+                else if(sayi == 5)
+                asalListe.Add(5);
+
+                else if(sayi == 7)
+                asalListe.Add(7);
+
+                else
+                antiasalListe.Add(sayi);
+            }
+            asalListe.Sort();
+            asalListe.Reverse();
+            Console.WriteLine("Asal Liste:");
+            foreach (var item in asalListe)
+            Console.WriteLine(item);
+
+            antiasalListe.Sort();
+            antiasalListe.Reverse();
+            Console.WriteLine("Asal olmayan liste:");
+            foreach (var item in antiasalListe)
+            Console.WriteLine(item);          
+
+            Console.WriteLine("Asal Sayı eleman sayısı : " + asalListe.Count);
+            Console.WriteLine("Asal Olmayan Sayı eleman sayısı : " + antiasalListe.Count);
+            
+            int sayac = 1;
+            int toplam = 0;
+            for (int i = 0; i < asalListe.Count ; i++)
+            {
+               toplam = toplam + Convert.ToInt32(asalListe[i]);
+                 sayac ++;
+            }
+            {
+                 
+            }
+            Console.WriteLine("asalların ortalaması:" +toplam/asalListe.Count);
+            int sayac2 = 1;
+            int toplam2 = 0;
+            for(int i=0; i<antiasalListe.Count ; i++)
+            {
+                 toplam2 = toplam2 + Convert.ToInt32(antiasalListe[i]);
+                 sayac2 ++;
+            }
+            Console.WriteLine("asal olmayanların ortalaması:" + toplam2/antiasalListe.Count);
+
+            
+
+        }
+    }
+}
