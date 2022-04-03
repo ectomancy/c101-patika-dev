b3668205c237ff0ffddd8d51fd22e682b870c5f4
ectomancy <98704895+ectomancy@users.noreply.github.com>
2022-04-04 00:13:12 +0300

    Sınıf Nedir ? Instance, Field, Property

---
+++ Sınıf Nedir  Instance Field Property/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sınıf_kavramı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Calısan calısan1 = new Calısan();
+            calısan1.Ad = "deniz";
+            calısan1.Soyad = "karayigit";
+            calısan1.No = 31;
+            calısan1.Departman = "İK";
+            calısan1.CalısanBilgileri();
+        }
+    }
+    class Calısan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+        public void CalısanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adı: {0}", Ad);
+            Console.WriteLine("Çalışan Soyadı: {0}", Soyad);
+            Console.WriteLine("Çalışan Numarası: {0}", No);
+            Console.WriteLine("Çalışan Departmanı: {0}", Departman);
+        }
+
+    }
+}
