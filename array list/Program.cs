f3fbf9cb30c67e7c18816d377a19104196896f0a
ectomancy <98704895+ectomancy@users.noreply.github.com>
2022-04-04 00:03:48 +0300

    c#101 soru 1

--- array list/Program.cs
+++ array list/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            liste.Add("Ayşe");
+            liste.Add(2);
+            liste.Add(true);
+            liste.Add('A');
+
+            Console.WriteLine(liste[1]);
+            foreach (var item in liste)
+              Console.WriteLine(item);
+
+            Console.WriteLine("sk,iiiiiiirttttt**************");
+            string[] renkler = {"kırmızı", "sarı", "yeşil"};
+            List<int> sayılar = new List<int>(){1,2,3,8,7,92,5};
+
+            liste.AddRange(sayılar);
+            liste.AddRange(renkler);  
+            foreach (var item in liste)
+            Console.WriteLine(item);
+            
+            
+            
+        }
+    }
+}
