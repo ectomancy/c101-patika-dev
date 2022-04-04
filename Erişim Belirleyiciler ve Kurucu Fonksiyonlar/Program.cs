953dbcf0514bf24a6c3709a139e232d4b9036c41
ectomancy <98704895+ectomancy@users.noreply.github.com>
2022-04-04 19:34:20 +0300

    Erişim Belirleyiciler ve Kurucu Fonksiyonlar

---
+++ Erişim Belirleyiciler ve Kurucu Fonksiyonlar/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sınıf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            adam adam1 = new adam("deniz","karayigit",1214213549,"aaa");
+            adam1.sorgulama();
+            Console.WriteLine("- - - - - - - - -");
+
+            adam adam2 = new adam();
+            adam2.name = "alex";
+            adam2.surname = "de souza";
+            adam2.id = 1905;
+            adam2.department = "bbbb";
+            adam2.sorgulama();
+            Console.WriteLine("- - - - - - - - -");
+
+            adam adam3 = new adam("ahmet","yılmaz");
+            adam3.sorgulama();
+
+            Console.ReadLine();
+        }
+        class adam
+        {
+            public int id;
+            public string name;
+            public string surname;
+            public string department;
+
+            public adam(string name, string surname, int id, string department){
+               this.name = name;
+               this.surname = surname;
+               this.id = id;
+               this.department = department;
+
+            }
+            public adam(string name, string surname){
+               this.name = name;
+               this.surname = surname;
+
+            }
+            public adam(){}
+
+            public void sorgulama()
+            {
+                Console.WriteLine("Employee name : {0}", name);
+                Console.WriteLine("Employee surname : {0}", surname);
+                Console.WriteLine("Employee id : {0}", id);
+                Console.WriteLine("Employee department : {0}", department);
+
+            }
+        }
+    }
+}
